a4a0bb1852da014f1a226665e376105bc5a9c87a
christinaaprilliani <avrillavril88@gmail.com>
2023-08-29 19:38:44 +0700

    ate a new repository on the command line

---
+++ todo_list_angular/src/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
+
+import { AppComponent } from './app.component';
+import { TodoComponent } from './todo/todo.component';
+import { TodoFormComponent } from './todo-form/todo-form.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    TodoComponent,
+    TodoFormComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ todo_list_angular/src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  todos: string[] = [];
+
+  onAddTodo(newTodo: string) {
+    this.todos.push(newTodo);
+  }
+
+  onRemoveTodo(index: number) {
+    this.todos.splice(index, 1);
+  }
+}
